7b51441e44f50bed27198df043ef5185e7c38a71
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-04-06 15:58:08 -0700

    fix folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/index.css';
-import App from './componenets/App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
